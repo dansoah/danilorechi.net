fa4abbeb283c7b0a6aa687770582e3e5b038be11
Danilo Rechi <fixmyprinter@danilorechi.net>
2023-05-16 01:46:58 +0000

    feat(blog-home): add header

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,20 +1,19 @@
 import { GetStaticProps } from 'next'
 import {  getBlogHomePosts } from '../lib/wordpress';
 import Link from 'next/link'
+import SiteHeader from '@/components/SiteHeader';
 
 export default function Blog({ allPosts: { edges }, preview }) {
     const heroPost = edges[0]?.node
     const morePosts = edges.slice(1)
 
-    console.log(edges);
-
     return (
         <main>
+            <SiteHeader />
             Blog:
             <ul>
             {
                 edges.map( edge => {
-                    console.log(edge);
                     return (<li key={edge.node.slug}> <Link href={"/posts/"+edge.node.slug}>{edge.node.title}</Link> </li>)
                 })
             }
