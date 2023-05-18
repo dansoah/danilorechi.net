c90b20b0eb02d68e688ce9838a71969f6436ed70
Danilo Rechi <fixmyprinter@danilorechi.net>
2023-05-18 04:10:06 +0000

    fix(blog): replace siteheader import

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,23 +1,27 @@
 import { GetStaticProps } from 'next'
 import {  getBlogHomePosts } from '../lib/wordpress';
 import Link from 'next/link'
-import SiteHeader from '@/components/SiteHeader';
+import { SiteHeader } from '@/components';
+import styles from '@/styles/Blog.module.scss';
 
 export default function Blog({ allPosts: { edges }, preview }) {
     const heroPost = edges[0]?.node
     const morePosts = edges.slice(1)
 
     return (
-        <main>
+        <main className={styles.main}>
             <SiteHeader />
-            Blog:
-            <ul>
-            {
-                edges.map( edge => {
-                    return (<li key={edge.node.slug}> <Link href={"/posts/"+edge.node.slug}>{edge.node.title}</Link> </li>)
-                })
-            }
-            </ul>
+            
+            <section>
+                <ul>
+                    {
+                        edges.map(edge => {
+                            return (<li key={edge.node.slug}> <Link href={"/posts/" + edge.node.slug}>{edge.node.title}</Link> </li>)
+                        })
+                    }
+                </ul>
+            </section>
+            
         </main>
     )
 }
